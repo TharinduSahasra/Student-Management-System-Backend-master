86d0cfd39934efaeed796cc7306744584a685629
TharinduSahasra <sahasrasenevirathne@gmail.com>
2024-11-11 11:28:02 +0530

    updated

--- Student-Management-System-Backend-master/CRUD-Application/src/main/java/Entity/Student.java
+++ Student-Management-System-Backend-master/CRUD-Application/src/main/java/Entity/Student.java
@@ -22,19 +22,18 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
    private long id;
 
-    @Column(name = "first_name")
+   
     private String firstName;
 
-    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
+   
     private String email;
 
-    @Column(name = "department")
+    
     private String department;
 
-    @Column(name = "year_of_enrollment")
+    
     private int yearOfEnrollment;
 
     
